504ae6f0277f8c0d0fde92559891dabc5263effa
EoinM95 <murphe43@tcd.ie>
2016-11-22 20:15:05 +0000

    Solved nan issue

--- src/vector_reader.py
+++ src/vector_reader.py
@@ -6,19 +6,25 @@ VECTOR_FILE = 'vectors.txt'
 def read_vectors_from_file(filename=VECTOR_FILE):
     """Read vectors from file, return dictionary"""
     vec_dictionary = {}
-    with open(filename, encoding='utf8') as stream:
+    with open(filename, 'rb') as stream:
         header = stream.readline()
         vocab_size, layer1_size = map(int, header.split())
         binary_len = np.dtype('float32').itemsize * layer1_size
+        print(vocab_size, binary_len)
         for i in range(vocab_size):
-            word = []
+            string_bytes = bytearray()
             while True:
-                char = stream.read(1)
+                next_byte = stream.read(1)
+                try:
+                    char = next_byte.decode('utf8')
+                except UnicodeDecodeError:
+                    string_bytes += bytearray(next_byte)
+                    continue
                 if char == ' ':
-                    word = ''.join(word)
+                    word = string_bytes.decode('utf8')
                     break
                 if char != '\n':
-                    word.append(char)
+                    string_bytes += bytearray(next_byte)
             vec_coords = np.fromstring(stream.read(binary_len), dtype='float32', count=layer1_size)
             next_vec = Vector(vec_coords)
             vec_dictionary[word] = next_vec
