48e0f299f2dedcb98bbe6fcc5ae2847f33dffa8c
EoinM95 <murphe43@tcd.ie>
2016-11-25 18:07:28 +0000

    Added functions to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 """Read a corpus, check there's a vector for everything"""
 from sentence_splitter import read_sentences_from_file
-from sentence import Sentence
+#from sentence import Sentence
 from vector_reader import read_vectors_from_file
-from vector import Vector
+#from vector import Vector
 
 def main():
     """Read data"""
@@ -10,3 +10,24 @@ def main():
     vector_dictionary = read_vectors_from_file(vector_file)
     text_file = input('Enter the filename of the text you wish to summarize')
     sentence_list = read_sentences_from_file(text_file)
+
+def ts_isf(sentence):
+    """Compute the mean """
+    print('TO DO')
+#need sentence position
+
+def similairty_to_title(sentence, title):
+    """Return simliarity of sentence to the title"""
+    return sentence.vector().cosine_similarity(title.vector())
+
+def similairty_to_keywords(sentence, keywords):
+    """Return similarity of sentence to keywords of text"""
+    print('TO DO')
+
+def compute_centroid(sentences):
+    """Return similarity of sentence to keywords of text"""
+    print('TO DO')
+
+def senetence_2_sentence_cohesion(sentences):
+    """Return similarity of sentence to keywords of text"""
+    print('TO DO')
--- src/sentence.py
+++ src/sentence.py
@@ -7,15 +7,24 @@ class Sentence(object):
     """docSentstring for Sentence class."""
     def __init__(self, text):
         self.text = text
+        self.s_vector = None
+        self.tokens = None
+        self.tokenize()
+        self.vector()
 
-    def sentence_vector(self):
+    def vector(self):
         """Will return vector which represents sentence"""
-        word_vec_list = []
-        for word in self.tokenize():
-            word_vec = Vector(word)
-            word_vec_list.append(word_vec)
-        return sum_of_vectors(word_vec_list)
+        if  not self.s_vector:
+            word_vec_list = []
+            for word in self.tokenize():
+                word_vec = Vector(word)
+                word_vec_list.append(word_vec)
+            self.s_vector = sum_of_vectors(word_vec_list)
+        return self.s_vector
+
 
     def tokenize(self):
         """Return list of tokens in sentence"""
-        return re.split(self.text, TOKEN_PATTERN)
+        if not self.tokens:
+            self.tokens = re.split(self.text, TOKEN_PATTERN)
+        return self.tokens
