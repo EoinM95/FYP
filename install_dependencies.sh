745a13e1b6ececece27edfcdd2a19ff82490a951
EoinM95 <murphe43@tcd.ie>
2017-03-25 17:35:23 +0000

    Fixed install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,5 @@
+sudo apt-get install --upgrade python3-dev python3-pip
+sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade numpy 
 sudo pip3 install --upgrade scipy
 sudo pip3 install --upgrade tensorflow scikit-learn nltk
